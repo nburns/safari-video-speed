e438f4aaa8006747f2ead71a61f6ebd191112727
Nick Burns <nick@burns.io>
2025-02-08 19:44:58 -0800

    format

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,9 +1,42 @@
+function getVideos() {
+  return Array.from(document.querySelectorAll("video"));
+}
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.speed) {
-        Array.from(document.querySelectorAll('video')).forEach(element => {
-            element.playbackRate = request.speed
-            console.log(request.speed);
-        });
-    }
+  const videos = getVideos();
+
+  if (request.speed) {
+    videos.forEach((element) => {
+      element.playbackRate = request.speed;
+      console.log("speed:", request.speed);
+    });
+  }
+  if (request.skip) {
+    videos.forEach((element) => {
+      element.currentTime += request.skip;
+      console.log("skip:", request.skip);
+    });
+  }
 });
 
+document.addEventListener("keydown", (event) => {
+  const videos = getVideos();
+
+  if (event.key == "j") {
+    videos.forEach((video) => {
+      video.currentTime -= 10;
+    });
+  }
+
+  if (event.key == "k") {
+    videos.forEach((video) => (video.paused ? video.play() : video.pause()));
+  }
+
+  if (event.key == "l") {
+    videos.forEach((video) => {
+      video.currentTime += 10;
+    });
+  }
+});
+
+console.log("hello");
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,30 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('input[type=range]').addEventListener("change", (event) => {
-        const {target} = event;
-        const {value} = target;
-        
-        const currentValueLabel = document.querySelector("#speed");
-        
-        currentValueLabel.innerText = `${value}x`;
-        
-        function sendMessageToTabs(tabs) {
-          for (const tab of tabs) {
-            browser.tabs
-              .sendMessage(tab.id, { speed: event.target.value })
-              .then(() => { })
-              .catch(console.error);
-          }
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector("input[type=range]")
+    .addEventListener("change", (event) => {
+      const { target } = event;
+      const { value } = target;
+
+      const currentValueLabel = document.querySelector("#speed");
+
+      currentValueLabel.innerText = `${value}x`;
+
+      function sendMessageToTabs(tabs) {
+        for (const tab of tabs) {
+          const value = event.target.value;
+          const type = event.target.dataset.control;
+          console.log({ value, type });
 
           browser.tabs
-            .query({
-              currentWindow: true,
-              active: true,
-            })
-            .then(sendMessageToTabs)
-            .catch(onError);
-        
-    })
-    
-    
-})
+            .sendMessage(tab.id, { [type]: value })
+            .then(() => {})
+            .catch(console.error);
+        }
+      }
+
+      browser.tabs
+        .query({
+          currentWindow: true,
+          active: true,
+        })
+        .then(sendMessageToTabs)
+        .catch(console.error);
+    });
+});
